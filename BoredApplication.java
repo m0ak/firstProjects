b97095ac78003515e081c0ffc91a2d687dd9362b
m0ak <90358263+m0ak@users.noreply.github.com>
2024-02-17 16:53:10 +0100

    Add files via upload

---
+++ BoredApplication.java
@@ -0,0 +1,16 @@
+package at.upstream_mobility.itacademy.bored;
+
+import org.jline.terminal.TerminalBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class BoredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoredApplication.class, args);
+	}
+	
+}
